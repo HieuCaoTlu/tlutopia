c8f419b99a01789c6d9216f20a2655bea16d8372
dT0210 <duthanh41025@gmail.com>
2024-01-18 20:17:36 +0700

    initalize

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <nav class="border-b-gray-50 border-b">
+        <ul class="">
+            <li>Books</li>
+            <li>Majors</li>
+            <li>Courses</li>
+            <li>Loans</li>
+            <li>Users</li>
+        </ul>
+    </nav>
+</body>
+</html>
