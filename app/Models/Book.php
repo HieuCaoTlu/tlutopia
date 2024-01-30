0a4054e3dacf76532f2987c4fe297847640aebaf
dT0210 <duthanh41025@gmail.com>
2024-01-30 23:08:20 +0700

    add Loan model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     protected $table = 'book';
     use HasFactory;
+
+    public function loans(): HasMany {
+        return $this->hasMany(Loan::class, 'book_id');
+    }
 }
---
+++ app/Models/Loan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Loan extends Model
+{
+    protected $table = 'loan';
+    use HasFactory;
+
+    public function book() : BelongsTo {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
