72a0b02d585d21a11a4d24a02cea73e7736286b8
dT0210 <duthanh41025@gmail.com>
2024-02-19 22:59:41 +0700

    user list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function list() {
+        return view('users.list', [
+            'userList' => User::paginate(10)
+        ]);
+    }
 }
