d4eaddf9a74c21dcd7e7402a339dd079a709e647
dT0210 <duthanh41025@gmail.com>
2024-02-21 17:25:26 +0700

    add loan search

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -7,9 +7,22 @@ use App\Models\Loan;
 
 class LoanController extends Controller
 {
-    public function list() {
+    public function list(Request $request) {
+        $query = $request['query'];
+        if ($query != null) {
+            if ($request['type'] == 'student') {
+                $loans = Loan::whereHas('user', function ($user) use ($query) {
+                    $user->where('student_code', 'LIKE', "%{$query}%")
+                        ->orWhere('student_name', 'LIKE', "%{$query}%");
+                });
+            } else {
+                $loans = Loan::where('book_title', 'LIKE', "%{$query}%");
+            }
+        } else {
+            $loans = Loan::orderBy('loan_date');
+        }
         return view('loans.list', [
-            'loanList' => Loan::orderBy('loan_date')->paginate(10)
+            'loanList' => $loans->paginate(10)
         ]);
     }
 
@@ -21,9 +34,17 @@ class LoanController extends Controller
 
     public function changeStatus(Request $request) {
         $loan = Loan::find($request->input('id'));
-        $loan->update([
+        $formFields = [
             'status' => $request->input('status')
-        ]);
-        return redirect()->back();
+        ];
+        
+        if ($request->input('status') == "Chưa trả sách") {
+            $formFields['pickup_date'] = date('Y/m/d', time());
+        }
+        if ($request->input('status') == "Đã trả sách") {
+            $formFields['return_date'] = date('Y/m/d', time());
+        }
+        $loan->update($formFields);
+        return redirect()->back()->with('message', 'Loan updated.');
     }
 }
--- resources/views/loans/list.blade.php
+++ resources/views/loans/list.blade.php
@@ -6,18 +6,25 @@
     <div class="w-full">
         <div class="mb-4 flex justify-between">
             <div class="font-semibold text-xl ">All loans</div>
+            <form action="/loan/list" method="GET">
+                <input type="text" id="query" name="query" class="border border-gray-500 rounded h-8 px-2" placeholder="Search...">
+                <select name="type" id="type" class="border border-gray-500 rounded h-8">
+                    <option value="student">By student</option>
+                    <option value="book">By book</option>
+                </select>
+            </form>
         </div>
         
         <div class="w-full">
             <table class="w-full border-collapse text-center border border-gray-500">
                 <thead>
                     <tr>
-                        <td class="border border-gray-500 overflow-hidden font-semibold">ID</td>
+                        <td class="border border-gray-500 font-semibold">ID</td>
                         <td class="border border-gray-500 overflow-hidden font-semibold">Student code</td>
                         <td class="border border-gray-500 overflow-hidden font-semibold">Student name</td>
-                        <td class="border border-gray-500 overflow-hidden font-semibold">Book title</td>
-                        <td class="border border-gray-500 overflow-hidden font-semibold">Loan date</td>
-                        <td class="border border-gray-500 overflow-hidden font-semibold">Status</td>
+                        <td class="border border-gray-500 sm:w-[500px] overflow-hidden font-semibold">Book title</td>
+                        <td class="border border-gray-500 font-semibold">Loan date</td>
+                        <td class="border border-gray-500 font-semibold">Status</td>
                         <td class="border border-gray-500"></td>
                     </tr>
                 </thead>
@@ -27,7 +34,7 @@
                             <td>{{$loan->id}}</td>
                             <td>{{$loan->user->student_code}}</td>
                             <td>{{$loan->user->student_name}}</td>
-                            <td>{{$loan->book->title}}</td>
+                            <td>{{$loan->book_title}}</td>
                             <td>{{$loan->loan_date}}</td>
                             <td>
                                 <form action="/loan/update" method="POST">
@@ -50,8 +57,12 @@
                             <td><a href="/loan/{{$loan->id}}" target="_blank" class="text-blue-600 hover:underline">Details</a></td>
                         </tr>
                     @endforeach
+                    
                 </tbody>
             </table>
+            @if ($loanList->total() == 0)
+                <div class="w-full text-center text-xl font-medium">No loans found</div>
+            @endif
             <div class="mt-3">{{$loanList->links()}}</div>
         </div>
     </div>
