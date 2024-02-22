e44a625fed750a88683191968efdd54d71276393
hieupahm2703 <117287526+hieupahm2703@users.noreply.github.com>
2024-02-22 20:08:15 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:tlutopia/model/cart.dart';
-import 'package:tlutopia/model/noti.dart';
 import 'package:tlutopia/model/schedule.dart';
 import 'package:tlutopia/model/user.dart';
 import 'package:tlutopia/screen/flow.dart';
 import 'screen/login/login.dart';
 import 'package:flutter/services.dart';
+import 'package:tlutopia/provider/provider.dart';
 
 void main() async {
   await initializeDateFormatting('vi_VN', null);
@@ -21,32 +21,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return User(
       child: Schedule(
         child: Cart(
-          child: NotiCenter(
-            child: MaterialApp(
-              onGenerateRoute: (settings) {
-                if (settings.name == '/flow') {
-                  return MaterialPageRoute(
-                    builder: (context) => const FlowScreen(),
-                  );
-                }
+          child: MaterialApp(
+            onGenerateRoute: (settings) {
+              if (settings.name == '/flow') {
                 return MaterialPageRoute(
-                  builder: (context) => const ErrorPage(),
+                  builder: (context) => const FlowScreen(),
                 );
-              },
-              theme: ThemeData(fontFamily: 'PlusJakartaSans'),
-              debugShowCheckedModeBanner: false,
-              home: const Scaffold(
-                body: LoginScreen(),
-              ),
+              }
+              return MaterialPageRoute(
+                builder: (context) => const ErrorPage(),
+              );
+            },
+            theme: ThemeData(fontFamily: 'PlusJakartaSans'),
+            debugShowCheckedModeBanner: false,
+              themeMode: ThemeMode.system,
+              //theme: MyThemes.lightTheme,
+              darkTheme: MyThemes.darkTheme,
+            home: const Scaffold(
+              body: LoginScreen(),
             ),
           ),
         ),
       ),
     );
   }
+
 }
 
 class ErrorPage extends StatelessWidget {
